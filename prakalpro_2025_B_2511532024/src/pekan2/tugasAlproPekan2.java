4ce57899f5f338d081aa6067cf07b40989a7e970
annisalayli109 <annisalayliramadhani@gmail.com>
2025-09-21 17:17:56 +0700

    Tugas Alpro Pekan 2

---
+++ prakalpro_2025_B_2511532024/src/pekan2/tugasAlproPekan2.java
@@ -0,0 +1,21 @@
+package pekan2;
+
+public class tugasAlproPekan2 {
+	
+	//Data Game Roblox
+	
+	public static void main(String[] args) {
+		int maxPemain = 100;		// Maksimal Pemain dalam suatu server atau game
+		float rating = 4.6f;		// Rating game tersebut (0.0 - 5.0)
+		char genre = 'A';			// 'A' = Adventure, 'S' = Simulator, 'H' = Horror
+		boolean populer = true;		// True = Populer, False = Tidak Populer
+		
+		System.out.println(" === Data Game Roblox === ");
+		System.out.println("Nama Game	: Mount Atin");
+		System.out.println("Maximum Pemain	: " + maxPemain);
+		System.out.println("Rating		: " + rating);
+		System.out.println("Genre Game	: " + genre);
+		System.out.println("Populer		: " + populer);
+	}
+
+}
