25490a030c368d3a1085b2391af6de613281c5bb
Annisa Layli Ramadhani <annisalayliramadhani@gmail.com>
2025-11-10 11:35:24 +0700

    Add files via upload

---
+++ prakalpro_2025_B_2511532024/src/pekan7_2511532024/PanggilMahasiswa_2511532024.java
@@ -0,0 +1,15 @@
+package pekan7_2511532024;
+
+public class PanggilMahasiswa_2511532024 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511532024 a= new Mahasiswa_2511532024();
+		a.setNim(11532024);
+		a.setNama("Annisa Layli Ramadhani");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+
+	}
+
+}
